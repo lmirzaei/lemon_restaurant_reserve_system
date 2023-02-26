c83bd52cfc89ae557c3811e4767f94f48d55ba6f
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2023-02-26 09:11:04 -0800

    Replace div element with react fragment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>"This import Leila Home page" </h1>
-    </div>
+    <React.Fragment className="App">
+    </React.Fragment>
   );
 }
 
