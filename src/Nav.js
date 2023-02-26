c75a7e5d38ca461d30ebb977928b2a2cb705abab
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2023-02-26 10:32:47 -0800

    Add website navigation to the Nav React Component

--- src/Nav.js
+++ src/Nav.js
@@ -1,3 +1,12 @@
 function Nav() {
-    return <h2>Hi, I am the navigation component, a react component!</h2>;
+    return (
+        <ul>
+            <a href="home.html"><li>Home</li></a>
+            <a href="about.html"><li>About</li></a>
+            <a href="menu.html"><li>Menu</li></a>
+            <a href="reservations.html"><li>Reservations</li></a>
+            <a href="onlineorder.html"><li>Online Order</li></a>
+            <a href="login.html"><li>Login</li></a>
+        </ul>
+    );
 }
