7fc1e04ba2aef26bc346e82f221bcc23e8bcdef6
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2023-06-01 09:51:28 -0700

    Update footer of web page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import restaurantFood from "../assets/images/restaurant.jpg";
+import "../styles/Footer.css"
 
 function Footer() {
     return (
-        <ul>
-            <a href="navigator.html"><li>Doormat Navigator</li></a>
-            <a href="about.html"><li>About</li></a>
-            <a href="socialmedialinks.html"><li>Social Media Links</li></a>
-        </ul>
+        <>
+            <div className="footer">
+
+                    <img src={restaurantFood} alt="restaurant patio" />
+
+
+                <div className="left-content">
+                    <ul>
+                        <a href="navigator.html"><li>Doormat Navigator</li></a>
+                        <a href="about.html"><li>About</li></a>
+                        <a href="socialmedialinks.html"><li>Social Media Links</li></a>
+                    </ul>
+                </div>
+            </div>
+        </>
     );
 }
 export default Footer;
